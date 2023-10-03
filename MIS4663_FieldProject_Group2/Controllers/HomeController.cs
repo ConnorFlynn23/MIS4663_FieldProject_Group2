067a9e84e4b4ec89506661c9ab9796bfc7d57430
ConnorFlynn23 <111476427+ConnorFlynn23@users.noreply.github.com>
2023-10-02 20:37:46 -0500

    Add project files.

---
+++ MIS4663_FieldProject_Group2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MIS4663_FieldProject_Group2.Data;
+
+namespace MIS4663_FieldProject_Group2.Controllers
+{
+    public class HomeController : Controller
+    {
+        DbCon db = new DbCon();
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MIS4663_FieldProject_Group2/Migrations/20231003012927_FieldDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MIS4663_FieldProject_Group2.Migrations
+{
+    /// <inheritdoc />
+    public partial class FieldDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
